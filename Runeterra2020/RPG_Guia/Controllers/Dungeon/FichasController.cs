c55c67adadbda29192c49cb534bf670696a0d119
ViniciusAugustoRR <vini.rios.rocha@gmail.com>
2020-01-03 19:58:36 -0300

    Login verificação feita

---
+++ Runeterra2020/RPG_Guia/Controllers/Dungeon/FichasController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RPG_Guia.Data;
+using RPG_Guia.Models;
+
+namespace RPG_Guia.Controllers.Dungeon
+{
+    public class FichasController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Fichas
+        public ActionResult Index()
+        {
+            var fichas = db.Fichas.Include(f => f.Classe).Include(f => f.Raça).Include(f => f.Territorio).Include(f => f.Usuario);
+            return View(fichas.ToList());
+        }
+
+        // GET: Fichas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ficha ficha = db.Fichas.Find(id);
+            if (ficha == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ficha);
+        }
+
+        // GET: Fichas/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClasseId = new SelectList(db.Classes, "ClasseId", "Descriçao");
+            ViewBag.RaçaId = new SelectList(db.Raças, "RaçaId", "Descriçao");
+            ViewBag.TerritorioId = new SelectList(db.Territorios, "TerritorioId", "Descriçao");
+            ViewBag.UsuarioId = new SelectList(db.Usuarios, "UsuarioId", "NomeUsuario");
+            return View();
+        }
+
+        // POST: Fichas/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FichaId,Nome,Antepassado,Tendencia,Personalidade,Ideal,Vinculo,Fraqueza,Caracteristica,Força,Inteligencia,Sabedoria,Destreza,Constituição,Carisma,UsuarioId,TerritorioId,RaçaId,ClasseId")] Ficha ficha)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Fichas.Add(ficha);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClasseId = new SelectList(db.Classes, "ClasseId", "Descriçao", ficha.ClasseId);
+            ViewBag.RaçaId = new SelectList(db.Raças, "RaçaId", "Descriçao", ficha.RaçaId);
+            ViewBag.TerritorioId = new SelectList(db.Territorios, "TerritorioId", "Descriçao", ficha.TerritorioId);
+            ViewBag.UsuarioId = new SelectList(db.Usuarios, "UsuarioId", "NomeUsuario", ficha.UsuarioId);
+            return View(ficha);
+        }
+
+        // GET: Fichas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ficha ficha = db.Fichas.Find(id);
+            if (ficha == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClasseId = new SelectList(db.Classes, "ClasseId", "Descriçao", ficha.ClasseId);
+            ViewBag.RaçaId = new SelectList(db.Raças, "RaçaId", "Descriçao", ficha.RaçaId);
+            ViewBag.TerritorioId = new SelectList(db.Territorios, "TerritorioId", "Descriçao", ficha.TerritorioId);
+            ViewBag.UsuarioId = new SelectList(db.Usuarios, "UsuarioId", "NomeUsuario", ficha.UsuarioId);
+            return View(ficha);
+        }
+
+        // POST: Fichas/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FichaId,Nome,Antepassado,Tendencia,Personalidade,Ideal,Vinculo,Fraqueza,Caracteristica,Força,Inteligencia,Sabedoria,Destreza,Constituição,Carisma,UsuarioId,TerritorioId,RaçaId,ClasseId")] Ficha ficha)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ficha).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClasseId = new SelectList(db.Classes, "ClasseId", "Descriçao", ficha.ClasseId);
+            ViewBag.RaçaId = new SelectList(db.Raças, "RaçaId", "Descriçao", ficha.RaçaId);
+            ViewBag.TerritorioId = new SelectList(db.Territorios, "TerritorioId", "Descriçao", ficha.TerritorioId);
+            ViewBag.UsuarioId = new SelectList(db.Usuarios, "UsuarioId", "NomeUsuario", ficha.UsuarioId);
+            return View(ficha);
+        }
+
+        // GET: Fichas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ficha ficha = db.Fichas.Find(id);
+            if (ficha == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ficha);
+        }
+
+        // POST: Fichas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ficha ficha = db.Fichas.Find(id);
+            db.Fichas.Remove(ficha);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Runeterra2020/RPG_Guia/Controllers/Dungeon/RaçaController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RPG_Guia.Data;
+using RPG_Guia.Models;
+
+namespace RPG_Guia.Controllers.Dungeon
+{
+    public class RaçaController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Raça
+        public ActionResult Index()
+        {
+            return View(db.Raças.ToList());
+        }
+
+        // GET: Raça/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Raça raça = db.Raças.Find(id);
+            if (raça == null)
+            {
+                return HttpNotFound();
+            }
+            return View(raça);
+        }
+
+        // GET: Raça/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Raça/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RaçaId,Descriçao,Nome")] Raça raça)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Raças.Add(raça);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(raça);
+        }
+
+        // GET: Raça/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Raça raça = db.Raças.Find(id);
+            if (raça == null)
+            {
+                return HttpNotFound();
+            }
+            return View(raça);
+        }
+
+        // POST: Raça/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RaçaId,Descriçao,Nome")] Raça raça)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(raça).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(raça);
+        }
+
+        // GET: Raça/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Raça raça = db.Raças.Find(id);
+            if (raça == null)
+            {
+                return HttpNotFound();
+            }
+            return View(raça);
+        }
+
+        // POST: Raça/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Raça raça = db.Raças.Find(id);
+            db.Raças.Remove(raça);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
